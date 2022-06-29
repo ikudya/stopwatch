0230ab659bdf3a65fb7c121e4a417dd8ef9fe8a5
Kudenets Mykhailo <mr.kudenecz@gmail.com>
2022-06-29 16:27:32 +0300

    Application stopwatch, dial (Seconds, Minutes) and control buttons: Start - starts the stopwatch. Stop - stops the stopwatch, resets the value to 0 Wait - Double-tap pauses the stopwatch. Restart - Resets the timer to 0 and automatically starts a new count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, buffer, filter, debounceTime } from 'rxjs/operators';
+import {Component} from "@angular/core";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent{
   title = 'stopwatch';
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+  startTimer: any;
+  running = false;
+
+  click$ = fromEvent(document, 'click');
+  doubleClick$ = this.click$
+    .pipe(
+      buffer(this.click$.pipe(debounceTime(500))),
+      map(clicks => clicks.length),
+      filter(clicksLength => clicksLength >= 2)
+    );
+
+
+  start(): void{
+    if (this.running != true) {
+      this.startTimer = setInterval(()=>{
+        this.running = true;
+        this.sec++
+        this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+
+        if(this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+         if(this.min === 60) {
+         this.stop();
+         alert('One hour has passed ')
+         }
+        }
+      }, 1000)
+    }
+    }
+  stop(): void {
+    this.running = false;
+    clearInterval(this.startTimer);
+    this.min = this.sec = '0' + 0;
+  }
+  reset():void {
+    if (this.running == true)
+    {
+      this.running = false;
+      clearInterval(this.startTimer);
+      this.min = this.sec = '0' + 0;
+      this.start();
+    } else {alert('To get started, start the stopwatch!')}
+
+  }
+
+  wait():void{
+    this.doubleClick$.subscribe(_ => {
+      this.running = false;
+      clearInterval(this.startTimer);
+    });
+  }
 }
